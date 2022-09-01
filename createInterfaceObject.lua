e87350d42c6d56b81daad294665e990a81f2394c
Woolac <112593171+Woolac@users.noreply.github.com>
2022-08-31 20:39:19 -0400

    Update createInterfaceObject.lua

--- createInterfaceObject.lua
+++ createInterfaceObject.lua
@@ -1,10 +1,10 @@
 return function(a1)
 	if not a1 then return end
-	print(a1, " SETTINGS")
+	
 	local objectType = a1[1]
 	local objectParent = a1[2]
-	local objectSize = a1[3]
-	local objectPosition = a1[4]
+	local objectPosition = a1[3]
+	local objectSize = a1[4]
 	local objectColor = a1[5]
 	local objectBevelAmount = a1[6]
 	local objectName = a1[7]
@@ -14,14 +14,14 @@ return function(a1)
 	
 
 	newObject.Name = objectName
-	newObject.AnchorPoint = UDim.new(.5,.5)
+	newObject.AnchorPoint = Vector2.new(.5,.5)
 	newObject.Position = objectPosition
 	newObject.Size = objectSize
 	
 	newBevel.CornerRadius = UDim.new(objectBevelAmount,0)
 	
 	if objectType == "Frame" then
-		newObject.BackgroundTransparency = objectColor
+		newObject.BackgroundColor3 = objectColor
 	end
 	
 	return newObject
