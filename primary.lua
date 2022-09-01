f41ed2d94166342257634d198f0d67a0033882d9
Woolac <112593171+Woolac@users.noreply.github.com>
2022-08-31 20:00:17 -0400

    Update primary.lua

--- primary.lua
+++ primary.lua
@@ -1,5 +1,58 @@
-local module = function()
-    local data = loadstring(game:HttpGet("https://raw.githubusercontent.com/Woolac/DULAC/main/test.lua"))()
-    print(data["fruits"])
+local plrs = game:GetService("Players")
+local player = plrs.LocalPlayer
+
+--//Services
+local getService = game:GetService("HttpGet")
+local RS = game:GetService("ReplicatedStorage")
+
+local primaryObject;
+local UI = {}
+local buttons = {}
+local modules = {}
+local utfCharacters = {}
+
+
+local smartKick = function(a1)
+	if not a1 then return end
+	player:Kick("[DULAC MESSAGE] (YOU ARE NOT BANNED): "..a1)
 end
-return module
+
+local setUTFCharacters = function()
+	for i = 32, 256 do
+		table.insert(utfCharacters,utf8.char(i))
+	end
+end
+
+local generateName = function()
+	if #utfCharacters <= 0 then smartKick("SOMETHING WENT WRONG PLEASE REJOIN") return end
+	local amount = math.random(5,30)
+	local value = ""
+	
+	for i = 1, amount, 1 do
+		if math.random(1,10) > 7 then
+			value = value..utfCharacters[math.random(1,#utfCharacters)]
+		else
+			value = value..math.random(1,1000)
+		end
+	end
+	
+	return value
+end
+
+local requireModule = function(a1)
+	if not a1 then return end
+	local module =  loadstring(getService(a1))
+	table.insert(modules,modules)
+end
+
+local createPrimaryUIObject = function(a1)
+	if not a1 then return end
+	local name = generateName()
+	local newObject = Instance.new("ScreenGui",a1)
+	newObject.Name = name
+	primaryObject = newObject
+end
+
+
+setUTFCharacters()
+createPrimaryUIObject(player.PlayerGui)
